4618ee03f4a037e40f65dd9fc4e2f0ad9d660042
vishalpandit <vishalpandit@cdac.in>
2024-02-07 18:47:15 +0530

    new t

--- src/main/java/com/cdac/curdoperation/entity/Users.java
+++ src/main/java/com/cdac/curdoperation/entity/Users.java
@@ -12,10 +12,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Users {
 
 	@Id
